5809c6e36f475e28c67c91f30a5f3b9588e1cd16
Serdar Olmez <serdarolmez156@gmail.com>
2022-03-22 17:22:37 +0100

    ex3 changes applied

--- 2-Browsers/Week1/homework/ex3-hijackLogo.js
+++ 2-Browsers/Week1/homework/ex3-hijackLogo.js
@@ -8,7 +8,7 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
    HackYourFuture logo instead.
 ------------------------------------------------------------------------------*/
 function hijackGoogleLogo() {
-  const hijack = document.querySelector('#logo');
+  const hijack = document.querySelector('.lnXdpd');
   hijack.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
   hijack.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
 }
